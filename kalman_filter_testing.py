8263316f05b502f94ec5e689cf2b2b9d73cc2e8b
curtis <curtispevans@gmail.com>
2025-02-27 12:48:15 -0700

    something weird is still going on

--- kalman_filter_testing.py
+++ kalman_filter_testing.py
@@ -9,11 +9,12 @@ bearings = np.load('data/bearing.npy')
 pixel_sizes = np.load('data/pixel_sizes.npy')
 true_distance = np.load('data/distances.npy')
 control = np.load('data/control.npy')
+relative_velocities = np.load('data/relative_velocity.npy')
 
 Ts = 1/30
 
-mu = jnp.array([jnp.cos(bearings[0]), jnp.sin(bearings[0]), pixel_sizes[0], 10, 10., 1./true_distance[0]])
-sigma = jnp.eye(6) * 0.1
+mu = jnp.array([jnp.cos(bearings[0]), jnp.sin(bearings[0]), pixel_sizes[0], relative_velocities[0,0], relative_velocities[0,1], 1./true_distance[0]])
+sigma = jnp.diag(jnp.array([jnp.cos(jnp.radians(0.1)), jnp.cos(jnp.radians(0.1)), 1, 1, 1, 1]))
 
 R = jnp.eye(6) * 0.1
 Q = jnp.eye(3) * 0.1
@@ -21,13 +22,17 @@ Q = jnp.eye(3) * 0.1
 est_dist = []
 est_bearing = []
 est_pixel_size = []
+est_relative_velocity_x = []
+est_relative_velocity_y = []
 
 for bearing, pixel_size, u in zip(bearings, pixel_sizes, control):
     measurement = jnp.array([jnp.cos(bearing), jnp.sin(bearing), pixel_size])
     mu, sigma = kalman_update(mu, sigma, u, measurement, R, Q, Ts)
-    est_dist.append(1/mu[5])
+    est_dist.append(np.abs(1/mu[5]))
     est_bearing.append(np.arctan2(mu[1], mu[0]))
     est_pixel_size.append(mu[2])
+    est_relative_velocity_x.append(mu[3])
+    est_relative_velocity_y.append(mu[4])
 
 
 
@@ -54,6 +59,16 @@ plt.plot(est_dist, label='Estimated Distance')
 plt.xlabel('Time')
 plt.ylabel('Distance')
 plt.title('Distance between Mavs')
+plt.legend()
+
+plt.subplot(224)
+plt.plot(relative_velocities[:,0], label='True Relative Velocity X')
+plt.plot(relative_velocities[:,1], label='True Relative Velocity Y')
+plt.plot(est_relative_velocity_x, label='Estimated Relative Velocity X')
+plt.plot(est_relative_velocity_y, label='Estimated Relative Velocity Y')
+plt.xlabel('Time')
+plt.ylabel('Relative Velocity')
+plt.title('Relative Velocity')
 plt.legend()
 
 plt.tight_layout()
--- visualizing_bearing.py
+++ visualizing_bearing.py
@@ -14,6 +14,7 @@ bearings_vel = []
 pixel_sizes = []
 distances = []
 control = []
+relative_velocities = []
 
 for i in range(500):
     mav1.update(-0.05)
@@ -22,13 +23,11 @@ for i in range(500):
     uy = mav1._state[3]*-0.05*np.cos(mav1._state[2])
     control.append(np.array([ux, uy]))
 
-    plt.plot(mav1._state[1], mav1._state[0], 'ro')
-    plt.plot(mav2._state[1], mav2._state[0], 'bo')
     bearing = np.arctan2(mav2._state[1] - mav1._state[1], mav2._state[0] - mav1._state[0])
     relative_bearing = (bearing - mav1._state[2]) % (2*np.pi)
     if relative_bearing > np.pi:
         relative_bearing -= 2*np.pi
-    bearings.append(relative_bearing - np.pi)
+    bearings.append(relative_bearing)
     if i > 0:
         bearings_vel.append((bearings[-1] - bearings[-2]) / Ts)
 
@@ -36,7 +35,14 @@ for i in range(500):
     pixel_size = 10 / rho
     pixel_sizes.append(pixel_size)
     distances.append(rho)
-    
+
+    rel_vel_own = mav1._state[3]*np.array([np.cos(mav1._state[2]), np.sin(mav1._state[2])])
+    rel_vel_intruder = mav2._state[3]*np.array([np.cos(mav2._state[2]), np.sin(mav2._state[2])])
+    relative_velocity = rel_vel_own - rel_vel_intruder
+    relative_velocities.append(relative_velocity)
+
+    plt.plot(mav1._state[1], mav1._state[0], 'ro')
+    plt.plot(mav2._state[1], mav2._state[0], 'bo')
     plt.title(f'Bearing between Mavs {np.rad2deg(relative_bearing)}')
     plt.pause(0.01)
 
@@ -77,3 +83,4 @@ np.save('data/bearing.npy', np.array(bearings))
 np.save('data/pixel_sizes.npy', np.array(pixel_sizes))
 np.save('data/distances.npy', np.array(distances))
 np.save('data/control.npy', np.array(control))
+np.save('data/relative_velocity.npy', np.array(relative_velocities))
