worked some stuff out and realized pixel_size derivative is wrong. Need to drop the 2

kalman_filter_testing.py
@@ -19,7 +19,7 @@ mu = jnp.array([jnp.cos(bearings[0]-np.radians(0.1)), jnp.sin(bearings[0]+np.rad
 sigma = jnp.diag(jnp.array([jnp.cos(jnp.radians(0.01)), jnp.cos(jnp.radians(0.01)), 1, .1, 0.1, 0.01]))
 
 R = jnp.diag(jnp.array([jnp.cos(jnp.radians(0.01)), jnp.cos(jnp.radians(0.01)), 1, 0.01, 0.01, 0.01]))
-Q = jnp.diag(jnp.array([jnp.cos(jnp.radians(0.01)), jnp.cos(jnp.radians(0.01)), 1, 0.0001]))
+Q = jnp.diag(jnp.array([jnp.cos(jnp.radians(0.01)), jnp.cos(jnp.radians(0.01)), 1]))
 
 est_dist = []
 est_bearing = []
@@ -28,7 +28,7 @@ est_relative_velocity_x = []
 est_relative_velocity_y = []
 
 for bearing, pixel_size, own_vel in zip(bearings, pixel_sizes, own_velocities):
-    measurement = jnp.array([jnp.cos(bearing), jnp.sin(bearing), pixel_size, 0])
+    measurement = jnp.array([jnp.cos(bearing), jnp.sin(bearing), pixel_size])
     mu, sigma = kalman_update(mu, sigma, own_vel, measurement, R, Q, Ts)
     est_dist.append(mu[5])
     est_bearing.append(np.arctan2(mu[1], mu[0]))

kalman_filtertildev_testing.py
@@ -2,7 +2,7 @@ import numpy as np
 import jax.numpy as jnp
 import matplotlib.pyplot as plt
 
-from models.kalman_filter_A import kalman_update
+from models.kalman_filter_vtilde import kalman_update
 from models.mav_dynamics import MavDynamics
 
 bearings = np.load('data/bearing.npy')
@@ -15,34 +15,30 @@ own_velocities = np.load('data/own_velocity.npy')
 Ts = 1/30
 intruder_vel = np.array([0., 30.])
 
-mu = jnp.array([jnp.cos(bearings[0]-np.radians(0.1)), jnp.sin(bearings[0]+np.radians(0.1)), 0.001, 0., 30., 1./true_distance[0], 20])
-sigma = jnp.diag(jnp.array([jnp.cos(jnp.radians(0.01)), jnp.cos(jnp.radians(0.01)), 1, .1, 0.1, 0.01, 10]))
+mu = jnp.array([jnp.cos(bearings[0]-np.radians(0.1)), jnp.sin(bearings[0]+np.radians(0.1)), pixel_sizes[0], 0., 0.])
+sigma = jnp.diag(jnp.array([jnp.cos(jnp.radians(0.01)), jnp.cos(jnp.radians(0.01)), 0.01, .1, .1]))
 
-Q = jnp.diag(jnp.array([jnp.cos(jnp.radians(0.01)), jnp.cos(jnp.radians(0.01)), 1, 0.01, 0.01, 0.1, 0.1]))
-R = jnp.diag(jnp.array([jnp.cos(jnp.radians(0.01)), jnp.cos(jnp.radians(0.01)), 1, 0.000001])) #, 0.000001]))
+R = jnp.diag(jnp.array([jnp.cos(jnp.radians(0.01)), jnp.cos(jnp.radians(0.01)), 0.001, 0.01, 0.01]))
+Q = jnp.diag(jnp.array([jnp.cos(jnp.radians(0.01)), jnp.cos(jnp.radians(0.01)), 1]))
 
 est_dist = []
 est_bearing = []
 est_pixel_size = []
 est_relative_velocity_x = []
 est_relative_velocity_y = []
-est_A = []
-
 
 for bearing, pixel_size, own_vel in zip(bearings, pixel_sizes, own_velocities):
-    measurement = jnp.array([jnp.cos(bearing), jnp.sin(bearing), pixel_size, 0]) #, 0])
-    mu, sigma = kalman_update(mu, sigma, own_vel, measurement, Q, R, Ts)
-    est_dist.append(mu[5])
+    measurement = jnp.array([jnp.cos(bearing), jnp.sin(bearing), pixel_size])
+    mu, sigma = kalman_update(mu, sigma, own_vel, measurement, R, Q, Ts)
+    # est_dist.append(mu[5])
     est_bearing.append(np.arctan2(mu[1], mu[0]))
     est_pixel_size.append(mu[2])
     est_relative_velocity_x.append(mu[3])
     est_relative_velocity_y.append(mu[4])
-    est_A.append(mu[6])
-    
 
 
 
-plt.subplot(221)
+plt.subplot(121)
 plt.plot(bearings, label='True Bearing')
 plt.plot(est_bearing, label='Estimated Bearing')
 plt.xlabel('Time')
@@ -50,7 +46,7 @@ plt.ylabel('Bearing')
 plt.legend()
 plt.title('Bearing between Mavs')
 
-plt.subplot(222)
+plt.subplot(122)
 plt.plot(pixel_sizes, label='True Pixel Size')
 plt.plot(est_pixel_size, label='Estimated Pixel Size')
 plt.xlabel('Time')
@@ -59,21 +55,13 @@ plt.title('Pixel Size')
 plt.legend()
 
 
-plt.subplot(223)
-plt.plot(1/true_distance, label='True inverse distance')
-plt.plot(est_dist, label='Estimated inverse distance')
-plt.xlabel('Time')
-plt.ylabel('Inverse Distance')
-plt.title('Inverse Distance between Mavs')
-plt.legend()
-
-plt.subplot(224)
-plt.plot(est_A, label='Estimated A')
-plt.plot(np.ones(len(est_A))*20, label='True A')
-plt.xlabel('Time')
-plt.ylabel('A')
-plt.title('A')
-plt.legend()
+# plt.subplot(223)
+# plt.plot(1/true_distance, label='True inverse distance')
+# plt.plot(est_dist, label='Estimated inverse distance')
+# plt.xlabel('Time')
+# plt.ylabel('Inverse Distance')
+# plt.title('Inverse Distance between Mavs')
+# plt.legend()
 
 
 # plt.subplot(224)
@@ -83,7 +71,6 @@ plt.legend()
 # plt.ylabel('Distance')
 # plt.title('Distance between Mavs')
 # plt.legend()
-
 
 # plt.subplot(224)
 # plt.plot(np.ones(len(est_relative_velocity_x))*intruder_vel[0], label='True Relative Velocity N')

models/kalman_filter.py
@@ -14,7 +14,7 @@ def motion_model(x, own_vel, delta_t):
     bearing_dot_relative_velocity = los_n*v_n + los_e*v_e
     f = jnp.array([eta*(los_e**2*v_n - los_n*los_e*v_e),
                    eta*(-los_n*los_e*v_n + los_n**2*v_e),
-                   -2*pixel_size*eta*bearing_dot_relative_velocity,
+                   -pixel_size*eta*bearing_dot_relative_velocity,
                    0,
                    0,
                    -eta**2*bearing_dot_relative_velocity])
@@ -36,7 +36,7 @@ def measurement_model(x, own_vel):
     v_n = c_n - own_vel[0]
     v_e = c_e - own_vel[1]
     bearing_dot_relative_velocity = los_n*v_n + los_e*v_e
-    return jnp.array([los_n, los_e, pixel_size, -pixel_size*bearing_dot_relative_velocity])
+    return jnp.array([los_n, los_e, pixel_size])
 
 def jacobian_measurement_model(x, own_vel):
     '''
@@ -60,6 +60,6 @@ def kalman_update(mu, sigma, own_vel, measurement, R, Q, delta_t):
     mu = mu_bar + K@(measurement - z)
     I = jnp.eye(len(K))
     sigma = (I - K@H)@sigma_bar@(I - K@H).T + K@Q@K.T
-    sigma = (jnp.eye(len(K)) - K@H)@sigma_bar
+    # sigma = (jnp.eye(len(K)) - K@H)@sigma_bar
     
     return mu, sigma 

models/kalman_filter_vtilde.py
@@ -4,17 +4,13 @@ import jax.numpy as jnp
 import jax
 
 def f(x, own_vel):
-    los_n, los_e, pixel_size, c_n, c_e, eta, A = x
-    v_n = c_n - own_vel[0]
-    v_e = c_e - own_vel[1]
+    los_n, los_e, pixel_size, v_n, v_e = x
     bearing_dot_relative_velocity = los_n*v_n + los_e*v_e
-    _f = jnp.array([eta*(los_e**2*v_n - los_n*los_e*v_e),
-                     eta*(-los_n*los_e*v_n + los_n**2*v_e),
-                     -pixel_size*eta*bearing_dot_relative_velocity,
-                     0,
-                     0,
-                     -eta**2*bearing_dot_relative_velocity,
-                    -0*pixel_size*bearing_dot_relative_velocity])
+    _f = jnp.array([(los_e**2*v_n - los_n*los_e*v_e),
+                     (-los_n*los_e*v_n + los_n**2*v_e),
+                     -2*pixel_size*bearing_dot_relative_velocity,
+                     -v_n*bearing_dot_relative_velocity,
+                     -v_e*bearing_dot_relative_velocity])
     return _f
 
 def motion_model(x, own_vel, delta_t):
@@ -38,11 +34,9 @@ def measurement_model(x, own_vel):
     '''
     x: state vector x=[los_x, los_y, pixel_area, relative_velocity_x, relative_velocity_y, inverse_distance]
     '''
-    los_n, los_e, pixel_size, c_n, c_e, eta, A = x
-    v_n = c_n - own_vel[0]
-    v_e = c_e - own_vel[1]
+    los_n, los_e, pixel_size, v_n, v_e = x
     bearing_dot_relative_velocity = los_n*v_n + los_e*v_e
-    return jnp.array([los_n, los_e, pixel_size, pixel_size - A*eta]) #, pixel_size*bearing_dot_relative_velocity])
+    return jnp.array([los_n, los_e, pixel_size]) #, pixel_size*bearing_dot_relative_velocity])
 
 def jacobian_measurement_model(x, own_vel):
     return jacfwd(measurement_model, argnums=0)(x, own_vel)

visualizing_bearing.py
@@ -34,7 +34,7 @@ for i in range(500):
         bearings_vel.append((bearings[-1] - bearings[-2]) / Ts)
 
     rho = np.linalg.norm(mav2._state[0:2] - mav1._state[0:2])
-    pixel_size = A / rho
+    pixel_size = A / rho 
     pixel_sizes.append(pixel_size)
     distances.append(rho)
 