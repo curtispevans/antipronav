hard coded a tangent line plotter to verify gradient and the gradient is not correct at the moment

jax_testing.py
@@ -11,6 +11,7 @@ num_scenarios = 1
 num_frames = 300
 plotting = False
 A = 16.42857142857143
+# A = 5
 
 all_bearings, all_pixel_sizes, all_true_distance, all_us, all_mav_states, true_As_vels, own_vels = get_simulated_data(Ts, num_scenarios, num_frames, False)
 
@@ -45,6 +46,8 @@ sigma_nca = np.eye(6)*1**2
 
 J = jacfwd(mht.wrapper_update_all_filters, argnums=12)
 
+gradients = []
+
 for j in range(len(bearings)-1):
     bearing = bearings[j+1]
     pixel_size = pixel_sizes[j+1]
@@ -58,19 +61,22 @@ for j in range(len(bearings)-1):
     
     jacobian_A = J(mu_mpc, sigma_mpc, mu_nca, sigma_nca, Q_mpc, R_mpc, Q_nca, R_nca, measurement, Ts, own_mav, u, A)
 
-    plt.plot(own_mav[1], own_mav[0], 'ro', label='Ownship Position')
-    plt.plot(mu_nca[1], mu_nca[0], 'bo', label='NCA Estimate Position')
-    true_pose = own_mav[:2] + np.array([np.cos(bearing + own_mav[2]), np.sin(bearing + own_mav[2])]) * true_distance[j+1]
-    plt.plot(true_pose[1], true_pose[0], 'go', label='True Intruder Position')
-    # plt.legend()
-    plt.title(f'Frame {j+1} - Intruder Position Estimate')
-    plt.xlabel('X Position (m)')
-    plt.ylabel('Y Position (m)')
-    plt.pause(0.01)
+    # plt.plot(own_mav[1], own_mav[0], 'ro', label='Ownship Position')
+    # plt.plot(mu_nca[1], mu_nca[0], 'bo', label='NCA Estimate Position')
+    # true_pose = own_mav[:2] + np.array([np.cos(bearing + own_mav[2]), np.sin(bearing + own_mav[2])]) * true_distance[j+1]
+    # plt.plot(true_pose[1], true_pose[0], 'go', label='True Intruder Position')
+    # # plt.legend()
+    # plt.title(f'Frame {j+1} - Intruder Position Estimate')
+    # plt.xlabel('X Position (m)')
+    # plt.ylabel('Y Position (m)')
+    # plt.pause(0.01)
 
-    print(D2)#, jacobian_A)
+    
+    gradients.append(jacobian_A)
+    # print(jacobian_A)
 
-plt.show()
+# plt.show()
+np.save('gradients.npy', np.array(gradients))
 
 
     

models/mht_A.py
@@ -138,7 +138,7 @@ def filter_state_measurement_probabilistic(intruders_dict, measurement, R, mahal
 
     return filtered_dict
 
-def filter_pose_measurement_probabilistic(intruders_dict, mav, R, mahalanobis_dist=1):
+def filter_pose_measurement_probabilistic(intruders_dict, mav, R, mahalanobis_dist, gradient):
     filtered_dict = {}
     mah_dists = []
     for A in list(intruders_dict.keys())[1:]:
@@ -151,7 +151,8 @@ def filter_pose_measurement_probabilistic(intruders_dict, mav, R, mahalanobis_di
 
         print_inno = False
         if 16 < A and A < 17:
-            print_inno = True
+            x = np.linspace(5, 40, 100)
+            y = 0.0*(x - A) + D2
 
         D2 = get_mahalanobis_distance_intruder_state(intruder_state, intruder_sigma, measurement_pos, R, print_inno)
         mah_dists.append(D2)
@@ -161,9 +162,12 @@ def filter_pose_measurement_probabilistic(intruders_dict, mav, R, mahalanobis_di
         #     filtered_dict[A] = [state, sigma, intruder_state, intruder_sigma]
     plt.figure(0)
     plt.plot(list(intruders_dict.keys())[1:], mah_dists, 'b-', label='Mahalanobis distances', alpha=0.05)
+    plt.plot(x, y, 'g-', alpha=0.05)
     plt.xlabel('Candidate A')
     plt.ylabel('Mahalanobis distance')
     plt.title('Mahalanobis distances of candidates')
+    plt.xlim(4, 40)
+    plt.ylim(-0.01, 0.5)
     plt.pause(0.01)       
     # plt.show()
 
@@ -255,8 +259,8 @@ def get_mahalanobis_distance_intruder_state(state, sigma, measurement, R, print_
     
     D2 = innovation.T @ np.linalg.inv(S) @ innovation
 
-    if print_inno:
-        print(D2)
+    # if print_inno:
+    #     print(D2)
     return D2
 
 

models/mht_A_jax.py
@@ -58,6 +58,6 @@ def get_mahalanobis_distance_intruder_state(state, sigma, measurement, R):
     S = C @ sigma @ C.T + R
     
     D2 = innovation.T @ jnp.linalg.inv(S) @ innovation
-    # print(innovation)
+    # print(D2)
     return D2
 

models/nearly_constant_accel_kf_jax.py
@@ -37,7 +37,7 @@ def kalman_update(mu, sigma, measurement, Q, R, Ts):
     # Prediction
     mu, F = f(mu, Ts)
     tol = jnp.linalg.det(sigma[-2:, -2:])
-    mu = barrier_function(mu, tol)
+    # mu = barrier_function(mu, tol)
     sigma = F @ sigma @ F.T + Q
 
     # Measurement update

monte_carlo_testing.py
@@ -22,6 +22,8 @@ last_pose_errors_adj = []
 scenario_true_As_min_dist = []
 scenario_true_As_voting = []
 
+gradients = np.load('gradients.npy')
+
 for i in tqdm(range(num_scenarios)):
     bearings = all_bearings[i]
     mav_states = all_mav_states[i]
@@ -85,7 +87,7 @@ for i in tqdm(range(num_scenarios)):
 
         # Filter candidates
         if j > 30:
-            intruders_dict = mht.filter_pose_measurement_probabilistic(intruders_dict, own_mav, R_nearly_constant_accel, 1)
+            intruders_dict = mht.filter_pose_measurement_probabilistic(intruders_dict, own_mav, R_nearly_constant_accel, 1, gradients[j])
             intruder_pose = mht.get_best_estimated_intruder_pose(intruders_dict)
             est_intruder_poses.append(intruder_pose)
             scenario_true_As_min_dist.append(intruders_dict['mah_dist_sorted'][0])